887299b47902bdf06252d24e2b54234ac848632f
Gabriel Salgueiro <gabrielsalgueiro@hotmail.com.br>
2019-12-04 10:29:07 -0300

    create interfaces

---
+++ Jogo_Imunogypti/Assets/Scripts/Interfaces/Interfaces.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Iattack
+{
+    
+}
+
+public interface Itarget
+{
+
+}
+
+public interface IEffect
+{
+
+}
