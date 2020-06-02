19827768eccba4a988f056cd613be5ecd1c89931
gabrielSalgueiro <gabrielsalgueiro@hotmail.com.br>
2020-06-02 02:18:41 -0300

    drawn path; Gamemannager and Base prepared

--- Jogo_Imunogypti/Assets/Scripts/Base.cs
+++ Jogo_Imunogypti/Assets/Scripts/Base.cs
@@ -6,67 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class Base : MonoBehaviour
 {
-    [SerializeField] private int hp;
-    [SerializeField] private int hpIni;
-    [SerializeField] private Text hpText;
-    public GameObject EndScreen;
-    public static Base instance; //Classe estática
-
-
-    void Awake()
-    {
-        if(instance!=null)
-        {
-            Debug.LogError("Mais de uma Base");
-            return;
-        }
-
-        instance = this;
-    }
-
-    void Start(){
-        hpIni = hp;
-    }
-
-    void Update() {
-        hpText.text = hp.ToString();    
-    }
-
-    public void TakeDamage(int damage)
-    {
-        hp -= damage;
-
-        if(hp <= 0)
-            Defeat();
-    }
-
-    public void Defeat()
-    {
-        Debug.Log("PErDi");
-         EndScreen.SetActive(true);
-         EndScreen.transform.GetChild(1).gameObject.SetActive(true);
-    }
-    public void Won(){
-        EndScreen.SetActive(true);
-         EndScreen.transform.GetChild(0).gameObject.SetActive(true);
-    }
-
     private void OnTriggerEnter(Collider other) 
     {
         Virus virus = other.gameObject.transform.parent.GetComponent<Virus>();
     
-        TakeDamage(virus.damage);
+        GameMannager.instance.TakeDamage(virus.damage);
     }
 
     public void BaseLocate(Vector3 locate)
     {
         transform.position = locate;
-    }
-
-    public int getHP(){
-        return hp;
-    }
-    public int getIHP(){
-        return hpIni;
     }
 }
--- Jogo_Imunogypti/Assets/Scripts/GameMannager.cs
+++ Jogo_Imunogypti/Assets/Scripts/GameMannager.cs
@@ -6,17 +6,22 @@ using System;
 using UnityEngine.UI;
 
 //classe que controla as principais funcoes do jogo
-public class SpawnPoint : MonoBehaviour
+public class GameMannager : MonoBehaviour
 {
+    [SerializeField] private Base[] bases;
+    [SerializeField] private int hp;
+    [SerializeField] private int hpIni;
+    [SerializeField] private Text hpText;
+    public GameObject EndScreen;
+
+    [SerializeField] private SpawnPoint[] spawnPoints;
     [SerializeField] private int wavesRemaning = 5; //marca quanntas ondas de inimigos faltam para o jogador vencer
     [SerializeField] private int waveNumber = 0; //marca qual a onda que o jogador esta enfrentando atualmente
-    // public List<List<Virus> > hordes; //guarda os inimigos que serao liberados para enfrentar o jogador
-    private List<List<Virus> > hordes = new List<List<Virus> >(); //guarda os inimigos que serao liberados para enfrentar o jogador
     [SerializeField] private float timeBetweenWaves = 5f; //tempo entre as ondas de inimigos
     [SerializeField] private float countdown = 7f; //marca tempo ate a proxima onda de ininmigos chegar
     public int activeViruses = 0; //marca quantos virus ainda estao ativos
     [SerializeField] private Virus[] virusPrefab;
-    public static SpawnPoint instance; //Classe estática
+    public static GameMannager instance; //Classe estática
     [SerializeField] private Text wavesRemaningText;
     [SerializeField] private Text timeRemaningNextWaveText;
 
@@ -24,7 +29,7 @@ public class SpawnPoint : MonoBehaviour
     {
         if(instance!=null)
         {
-            Debug.LogError("Mais de um SpawnPoint");
+            Debug.LogError("Mais de um GameMannager");
             return;
         }
 
@@ -37,25 +42,12 @@ public class SpawnPoint : MonoBehaviour
         waveNumber = 0;
         activeViruses = 0;
 
-        //seta as ondas de inimigos
-        for(int i = 0; i < wavesRemaning; i++)
-        {
-            List<Virus> aux = new List<Virus>();
-            for(int j = i*i + 5; j > 0; j--)
-            {
-                aux.Add(virusPrefab[0]);
-            }
-
-            for(int j = Math.Max(0, i-1); j > 0; j--)
-            {
-                aux.Add(virusPrefab[1]);
-            }
-            hordes.Add(aux);
-        }
+        hpIni = hp;
     }
 
     void Update()
     {
+        hpText.text = hp.ToString(); 
         wavesRemaningText.text = wavesRemaning.ToString(); 
         timeRemaningNextWaveText.text = Mathf.FloorToInt(countdown).ToString(); 
 
@@ -68,43 +60,53 @@ public class SpawnPoint : MonoBehaviour
         //verifica se esta na hora de mandar a proxima onda de inimigos
         if(countdown <= 0 && wavesRemaning > 0)
         {
-            //StartCoroutine(SpawnWave(hordes[waveNumber]));
-            StartCoroutine(SpawnWave(hordes[waveNumber]));
-            // StartCoroutine(SpawnWave());
+            foreach (SpawnPoint sp in spawnPoints){
+                StartCoroutine(sp.SpawnWave());
+                // StartCoroutine(SpawnWave(hordes[waveNumber]));
+            }
+            
             countdown = timeBetweenWaves+waveNumber;
             wavesRemaning--;
             waveNumber++;
         }    
-
-        if(countdown > 0)
+        else
             countdown -= Time.deltaTime; //conta tempo para mandar proxima onda de inimigos
     }
 
-    //funcao que libera uma onda de inimigos
-    public IEnumerator SpawnWave(List<Virus> horde)
-    // IEnumerator SpawnWave()
+    public void TakeDamage(int damage)
     {
-        activeViruses += horde.Count; //coloca todos os inimigos que serao estanciados nessa onda como ativos
+        hp -= damage;
 
-        //estancia os inimigos dessa onda
-        foreach (Virus virus in horde)
-        {
-            Vector3 rotation = new Vector3(-90f,0.4f,0);
-            Instantiate(virus, gameObject.transform.position, Quaternion.Euler(rotation));
-            yield return new WaitForSeconds(0.3f); //tempo entre a instanciacao de cada inimigo
-        }
+        if(hp <= 0)
+            Defeat();
     }
 
-    public void CallNextWave()
+    public void Defeat()
     {
-        countdown = 0;
+        Debug.Log("PErDi");
+        EndScreen.SetActive(true);
+        EndScreen.transform.GetChild(1).gameObject.SetActive(true);
     }
 
     //funcao de vitoria do jogador
     public void Win()
     {
         Debug.Log("voce venceu, PARABAINS!!!");
-        Base.instance.Won();
+        EndScreen.SetActive(true);
+        EndScreen.transform.GetChild(0).gameObject.SetActive(true);
     }
-}
 
+    public int getHP(){
+        return hp;
+    }
+    
+    public int getIHP(){
+        return hpIni;
+    }
+    
+    public void CallNextWave()
+    {
+        countdown = 0;
+    }
+
+}
