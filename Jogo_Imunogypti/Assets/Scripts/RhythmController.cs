f322723e0f846491da3d25aaa0d8e836ff0778ee
Alisson Gonsales <gonsalesalisson@usp.br>
2020-06-30 19:55:20 -0300

    clock atualizado

--- Jogo_Imunogypti/Assets/Scripts/RhythmController.cs
+++ Jogo_Imunogypti/Assets/Scripts/RhythmController.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RhythmController : MonoBehaviour
 {
-    private float timeScale = 1;
+    private float itimeScale = 1;
 
-    public void ChangeRhythm()
+    public void ChangeRhythm(Text scaleChange)
     {
-        Time.timeScale = timeScale = Time.timeScale%3 + 1;
+    	float change;
+    	switch(scaleChange.text){
+    		case "2x":
+    			change = 2.0f;
+                scaleChange.text = "0x";
+    			break;
+    		case "0.5x":
+    			change = 0.5f;
+                scaleChange.text = "1.5x";
+    			break;
+    		case "1.5x":
+    			change = 1.5f;
+                scaleChange.text = "2x";
+    			break;
+    		case "0x":
+    			change = 0.0f;
+                scaleChange.text = "0.5x";
+    			break;
+            default:
+                change = 1.0f;
+                break;
+
+    	}
+
+        Time.timeScale = itimeScale;
+        Time.timeScale = Time.timeScale*change;
     }
 
     public void ReturnRhythm()
     {
-        Time.timeScale = timeScale;
+        Time.timeScale = itimeScale;
     }
 }
