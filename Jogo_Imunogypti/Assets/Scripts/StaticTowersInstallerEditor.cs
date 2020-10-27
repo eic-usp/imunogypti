4e04554f936e96ebc2bd9251eef0963558b25245
LSatoshi <leandro.satoshi@usp.br>
2020-10-27 17:35:40 -0300

    hotfix

--- Jogo_Imunogypti/Assets/Scripts/StaticTowersInstallerEditor.cs
+++ Jogo_Imunogypti/Assets/Scripts/StaticTowersInstallerEditor.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -23,3 +23,4 @@ public class StaticTowersInstallerEditor : Editor
         }
     }
 }
+*/
