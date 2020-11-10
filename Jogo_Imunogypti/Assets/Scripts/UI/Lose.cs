0002fedf4e2eb7ef6e91e34af2a932474b2c6650
LSatoshi <leandro.satoshi@usp.br>
2020-11-09 22:24:50 -0300

    new stage added

--- Jogo_Imunogypti/Assets/Scripts/UI/Lose.cs
+++ Jogo_Imunogypti/Assets/Scripts/UI/Lose.cs
@@ -25,46 +25,6 @@ public class Lose : MonoBehaviour
         DoLittleStars(R);
     }
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    /*public void CalculateHeights(int fHP,int iHP){
-
-        if(fHP==null || iHP == null){
-          return;
-        }
-
-        R = ((float)fHP/iHP);
-        float H = 0;
-        float Hl = 0;
-        float Hg = 0;
-        h = 0;
-        int i=0;
-
-        foreach(GameObject star in Stars){
-            RectTransform rt = (RectTransform)star.transform;
-            Hl = rt.rect.height * rt.localScale.y;
-            if(i==0){
-                Hg = Hl;
-            }
-            H += Hl;
-            if(i<Stars.Count)
-                i++;
-        }
-        h = R * H *100;
-        Debug.Log(R);
-        DoLittleStars(Hl,Hg);
-
-    }*/
-
     public void DoLittleStars(float Q){
         
         if(Q==null){
@@ -77,19 +37,6 @@ public class Lose : MonoBehaviour
             Stars[i].transform.GetComponent<SVGImage>().sprite = goldStar;
             Stars[i].transform.GetComponent<LittleStars>().filled = true;
         }
-        //Debug.Log("Subtract " +(int)Q *100 * Hl);
-        //h = h - (int)Q * 100 *Hl;
-        //Debug.Log("h final " + h);
-        //Points.text = ((int)(Q)).ToString()+"%";
-    }
-
-    public void Play(){
-        SceneManager.LoadScene("BaseLevel");
-        this.transform.parent.gameObject.SetActive(false);
-    }
-    public void Return(){
-        SceneManager.LoadScene("MainMenu");
-        this.transform.parent.gameObject.SetActive(false);
     }
 
 }
