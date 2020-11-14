93a7df6f2d0cba67ddea194f49a0241b25195afa
LSatoshi <leandro.satoshi@usp.br>
2020-11-14 11:55:43 -0300

    all stages completed with tutorial

---
+++ Jogo_Imunogypti/Assets/Scripts/UI/UnlockLevels.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockLevels : MonoBehaviour {
+    public Button[] levels;
+
+    // Start is called before the first frame update
+    void Start() {
+        for(int i = 0; i < 8; i++) {
+            if(SaveLoader.saveFile.stagesWon[i]) {
+                levels[i].interactable = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
